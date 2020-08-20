733b507132f5ca9d6de971012831663ccea93f4a
Andres Pontt <none@none.com>
2020-08-20 14:29:21 -0400

    change to material ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CovidDashboard from './CovidDashboard';
+import {Typography, Link, Container, Box } from '@material-ui/core';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com"/>
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <CovidDashboard />
-      </header>
-    </div>
+   <Container maxWidth="sm">
+     <Box my={4}>
+       <Typography variant="h4" component="h1" gutterBottom>
+         andrespontt.github.io
+       </Typography>
+       <Copyright />
+       <CovidDashboard />
+     </Box>
+   </Container>
   );
 }
 
--- src/CovidDashboard.js
+++ src/CovidDashboard.js
@@ -1,49 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import {TableContainer, Paper} from '@material-ui/core';
+import classes from '*.module.css';
 
 export default function MyComponent() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-  
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect(() => {
-      fetch("https://raw.githubusercontent.com/andrespontt/data/master/covid-current.json")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result);
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/andrespontt/data/master/covid-current.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table"></Table>
+        </TableContainer>
         <table>
-            <tr>
-                <td>a</td>
-                <td>b</td>
-            </tr>
+          <tr>
+            <td>a</td>
+            <td>b</td>
+          </tr>
           {items.map(item => (
             <tr key={item.date}>
-              <td>{item.state}</td> 
+              <td>{item.state}</td>
               <td>{item.positive}</td>
             </tr>
           ))}
         </table>
-      );
-    }
+      </div>
+
+    );
   }
+}
