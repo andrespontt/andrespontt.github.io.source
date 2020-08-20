abd408e9b81838dbe1e3f1f21bf9f9e44d7ea909
Andres Pontt <none@none.com>
2020-08-19 23:35:51 -0400

    init project

---
+++ build/precache-manifest.331bb57e38d0c625aa38d3ae9534db7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2273bcffa48597011f547870b1c83833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a258e7b0125b9182774a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22e8cdce55c61da33110",
+    "url": "/static/js/2.4e76770c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e76770c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a258e7b0125b9182774a",
+    "url": "/static/js/main.ca934a71.chunk.js"
+  },
+  {
+    "revision": "fb8cfbe2e5f29fa37fc0",
+    "url": "/static/js/runtime-main.717f4f89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/CovidDashboard.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+export default function MyComponent() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+      fetch("https://raw.githubusercontent.com/andrespontt/data/master/covid-current.json")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <table>
+            <tr>
+                <td>a</td>
+                <td>b</td>
+            </tr>
+          {items.map(item => (
+            <tr key={item.date}>
+              <td>{item.state}</td> 
+              <td>{item.positive}</td>
+            </tr>
+          ))}
+        </table>
+      );
+    }
+  }
