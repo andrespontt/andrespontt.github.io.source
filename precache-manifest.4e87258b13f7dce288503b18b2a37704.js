9483cf2763dc9a9423b4ee12d03f54709ebeccf0
andrespontt <andrespontt@gmail.com>
2023-07-26 00:02:38 +0000

    Updates

---
+++ precache-manifest.4e87258b13f7dce288503b18b2a37704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb49bfef31a3cc5a8bcb5ade77bfc2e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147de184cfcb3149306c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4ec421b4cb400f0e4a5",
+    "url": "/static/js/2.1b2199d1.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "/static/js/2.1b2199d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147de184cfcb3149306c",
+    "url": "/static/js/main.29209ae6.chunk.js"
+  },
+  {
+    "revision": "6cb6ff9f918c448a2dd5",
+    "url": "/static/js/runtime-main.67585a02.js"
+  }
+]);
